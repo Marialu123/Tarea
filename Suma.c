28c7c3779e68a68be14155b9cf6e98a5abd96b01
Slackware Live User <live@darkstar.example.net>
2016-08-18 16:54:09 -0700

    archivo por compilar

---
+++ Suma.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b,suma;
+  printf("\n ingrese numero a:");
+  scanf( "%d", &a);
+  printf( "\n ingrese numero b:");
+  scanf( "%d", &b);
+  printf("\n la suma es: %d", a + b );
+  return 0;
+}
+
+  
